344cce5a39200deba78984ca6044b3062573b881
dhanush <dhanush.dhoni007@gmail.com>
2025-06-12 21:15:22 +0530

    change

---
+++ mail.php
@@ -0,0 +1,21 @@
+<?php
+
+$name = $_POST['name'] ?? '';
+$email = $_POST['email'] ?? '';
+$message = $_POST['message'] ?? '';
+$number = $_POST['phone'] ?? ''; 
+
+$to = ""; 
+
+$subject = "New contact from $name";
+$txt = "Name: $name\nPhone: $number\nEmail: $email\nMessage:\n$message";
+$headers = "From: $email";
+
+if (!empty($email) && !empty($message)) {
+    mail($to, $subject, $txt, $headers);
+}
+
+header("Location: thankyou.html");
+exit;
+
+?>
